368afdd42fa22b54961bf6ae0462663301e34511
ABHIRAM-DEVP <abhiramdevp21@gmail.com>
2026-07-19 12:23:58 +0530

    Add files via upload

---
+++ JULY192026.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class JULY192026{
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+        return null;
+    }
+
+    // Swap the left and right children using recursion
+    TreeNode temp = root.left;
+    root.left = invertTree(root.right);
+    root.right = invertTree(temp);
+
+    return root;
+    }
+}
+
+//time complexity: O(N)
+//space complexity: O(H)
