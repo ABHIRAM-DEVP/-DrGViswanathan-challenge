7866758efe8fb7145553583f00261cf95a80e989
ABHIRAM-DEVP <abhiramdevp21@gmail.com>
2026-07-20 07:20:02 +0530

    Add files via upload

---
+++ JULY202026.java
@@ -0,0 +1,27 @@
+public class JULY202026{
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public int maxDepth(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+        
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+        
+        // The depth of the current node is 1 + the greater of the two depths
+        return Math.max(leftDepth, rightDepth) + 1;
+    }
+}
+//time complexity: O(N)
+//space complexity: O(H)
