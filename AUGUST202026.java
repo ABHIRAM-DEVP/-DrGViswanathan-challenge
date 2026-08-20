6753605c19d44fe2506172c8b5638c4cb8f4c0bc
ABHIRAM-DEVP <abhiramdevp21@gmail.com>
2026-08-20 07:26:12 +0530

    Add files via upload

---
+++ AUGUST202026.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class TreeNode {
+     int val;
+     TreeNode left;
+    TreeNode right;
+     TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+     TreeNode(int val, TreeNode left, TreeNode right) {
+         this.val = val;
+         this.left = left;
+        this.right = right;
+      }
+}
+public class AUGUST202026{
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if(root == null){
+            return false;
+        }
+        targetSum -= root.val;
+
+        if(root.left == null && root.right == null){
+            return targetSum == 0;
+        }
+
+        return hasPathSum(root.left, targetSum) || hasPathSum(root.right, targetSum);
+    }
+}
+
+//time complexity: O(n)
+//space complexity: O(h)
